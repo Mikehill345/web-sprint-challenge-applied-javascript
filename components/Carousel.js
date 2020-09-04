72dffd9caaffe26d9193c76bb7ad5ca4db44faeb
mike hill <michael-hill@lambdastudents.com>
2020-09-04 13:45:49 -0400

    did all the js for normal questions and attempted stretch

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,42 @@
     <div class="right-button"> > </div>
   </div>
 */
+const carouselContainer = document.querySelector('.carousel-container')
+
+const imgArry = ["https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg","https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg",
+"https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg","https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg"]
+
+function carousel(){
+
+
+  let slideIndex = 0;
+  const carousel = document.createElement('div')
+  const leftButton = document.createElement('div')
+  const imgOne = document.createElement('img')
+  const rightButton = document.createElement('div')
+
+  carousel.appendChild(leftButton)
+  carousel.appendChild(imgOne)
+  carousel.appendChild(rightButton)
+
+  carousel.classList.add('carousel')
+  leftButton.classList.add('left-button')
+  rightButton.classList.add('right-button')
+  imgOne.src = imgArry[slideIndex]
+  imgOne.style.display = 'block'
+ 
+
+
+  rightButton.addEventListener('click',event =>{
+   imgOne.src = imgArry[slideIndex + 1]
+  })
+ leftButton.addEventListener('click',event =>{
+  imgOne.src = imgArry[slideIndex - 1]
+ })
+
+ return carousel
+}
+
+
+carouselContainer.appendChild(carousel())
+
